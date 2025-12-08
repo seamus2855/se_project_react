10335a272c910cbd5e8be77e3f1bc29c6e734fac
Seamus Ryan <seamusryan787@gmail.com>
2025-12-08 04:14:09 -0500

    finish sprint 10

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,10 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,   // run dev server on port 3000
-    open: true,   // auto-open browser when you run `npm run dev`
+    port: 3000, // run dev server on port 3000
+    open: true, // auto-open browser when you run `npm run dev`
   },
   preview: {
-    port: 3000,   // also use port 3000 for `npm run preview`
+    port: 3000, // also use port 3000 for `npm run preview`
   },
 });
