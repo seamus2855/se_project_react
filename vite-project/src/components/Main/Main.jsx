47fa88e51835128c99555d2f743dc30cc0968533
Seamus Ryan <seamusryan787@gmail.com>
2025-12-09 18:11:31 -0500

    finish

--- vite-project/src/components/Main/Main.jsx
+++ vite-project/src/components/Main/Main.jsx
@@ -2,7 +2,7 @@ import "./Main.css";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../vite-project/src/ItemCard/ItemCard";
 import { defaultClothingItems } from "../../utils/constants";
-
+import App from "./App.jsx";
 function Main({ weatherData, handleCardClick }) {
   return (
     <main>
