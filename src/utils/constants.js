8d57fca22bdbab79a4d44fd57523f25f8866ce0a
Seamus Ryan <seamusryan787@gmail.com>
2025-12-15 20:59:08 -0500

    src/utils/constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,62 +4,61 @@ export const weatherOptions = [
     condition: "clear",
     url: new URL("../assets/day/clear.svg", import.meta.url),
   },
-{
-  day: true,
-  condition: "cloudy",
-  url: new URL("../assets/day/cloudy%20day.svg", import.meta.url.href),
-},
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("../assets/day/cloudyday.svg", import.meta.url.href),
+  },
 
-
-{
-  day: true,
-  condition: "rain",
-  url: new URL("../assets/day/Rain%20Day.svg" , import.meta.url.href),
-},
+  {
+    day: true,
+    condition: "rain",
+    url: new URL("../assets/day/rainday.svg", import.meta.url.href),
+  },
   {
     day: true,
     condition: "snow",
-    url: new URL("../assets/day/snow%20day.svg", import.meta.url.href),
+    url: new URL("../assets/day/snowday.svg", import.meta.url.href),
   },
   {
     day: true,
     condition: "storm",
-    url: new URL("../assets/day/storm%20day.svg", import.meta.url.href),
+    url: new URL("../assets/day/stormday.svg", import.meta.url.href),
   },
   {
     day: true,
     condition: "Fog",
-    url: new URL("../assets/day/Fog%20day.svg", import.meta.url.href),
+    url: new URL("../assets/day/fogday.svg", import.meta.url.href),
   },
   {
     day: false,
     condition: "clear",
-    url: new URL("../assets/night/clear%20night.svg", import.meta.url.href),
+    url: new URL("../assets/night/clearnight.svg", import.meta.url.href),
   },
   {
     day: false,
     condition: "cloudy",
-    url: new URL("../assets/night/cloudy%20night.svg", import.meta.url.href),
+    url: new URL("../assets/night/cloudynight.svg", import.meta.url.href),
   },
   {
     day: false,
     condition: "rain",
-    url: new URL("../assets/night/rain%20night.svg", import.meta.url.href),
+    url: new URL("../assets/night/rainnight.svg", import.meta.url.href),
   },
   {
     day: false,
     condition: "snow",
-    url: new URL("../assets/night/snow%20night.svg", import.meta.url.href),
+    url: new URL("../assets/night/snownight.svg", import.meta.url.href),
   },
   {
     day: false,
     condition: "storm",
-    url: new URL("../assets/night/storm%20night.svg", import.meta.url.href),
+    url: new URL("../assets/night/stormnight.svg", import.meta.url.href),
   },
   {
     day: false,
     condition: "Fog",
-    url: new URL("../assets/night/fog%20night.svg", import.meta.url.href),
+    url: new URL("../assets/night/fognight.svg", import.meta.url.href),
   },
 ];
 
