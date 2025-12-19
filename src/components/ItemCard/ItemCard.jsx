a2832363449e441a0f4b15f6344f5421d6220c1d
Seamus Ryan <seamusryan787@gmail.com>
2025-12-18 20:22:37 -0500

    src/components/ItemCard/*

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,19 +1,27 @@
+
 import "./ItemCard.css";
 
 function ItemCard({ item, onCardClick }) {
-
   const handleCardClick = () => {
     onCardClick(item);
   };
 
   return (
-    <li>
+    <li className="card" onClick={handleCardClick}>
       <img
-        onClick={handleCardClick}
         className="card__image"
-        src={item.link}
+        src={item.link || item.imageUrl}
         alt={item.name}
       />
+      <div className="card__content">
+        <h2 className="card__name">{item.name}</h2>
+        {item.weather && (
+          <p className="card__weather">{item.weather}</p>
+        )}
+        {item.description && (
+          <p className="card__description">{item.description}</p>
+        )}
+      </div>
     </li>
   );
 }
