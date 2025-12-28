34f1cea33a9b91a486fc5ad7f809c72b8392ef7a
Seamus Ryan <seamusryan787@gmail.com>
2025-12-27 19:12:26 -0500

    src

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,11 @@
-import WeatherCard from "./components/WeatherCard/WeatherCard";
-import ItemCard from "./components/ItemCard/ItemCard";
-import "./components/Main/Main";
+import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import "./Main.css"; // if you have a CSS file for Main
 
 function Main({ weatherData, clothingItems, onCardClick }) {
-  // Determine weather type
   const temp = weatherData.temp.F;
   const weatherType = temp >= 86 ? "hot" : temp >= 66 ? "warm" : "cold";
 
-  // Filter items by weather
   const filteredItems = clothingItems.filter(
     (item) => item.weather === weatherType
   );
@@ -15,10 +13,12 @@ function Main({ weatherData, clothingItems, onCardClick }) {
   return (
     <main>
       <WeatherCard weatherData={weatherData} />
+
       <section className="cards">
         <p className="cards__text">
           Today is {temp}&deg;F / You may want to wear:
         </p>
+
         <ul className="cards__list">
           {filteredItems.map((item) => (
             <ItemCard key={item._id} item={item} onCardClick={onCardClick} />
