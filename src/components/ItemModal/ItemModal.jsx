ca72ca0c38b2eaeec1942c2fb5fa7831bdfee0cf
Seamus Ryan <seamusryan787@gmail.com>
2026-01-05 01:40:40 -0500

    src

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./components/ItemModal.css";
+import "./components/ItemModal/ItemModal.css";
 import "./components/WeatherImage.css";
 
 export default function ClothesModal({
