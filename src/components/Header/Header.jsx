07a017fe79770e1802ea17f90b1de48f362f7064
Seamus Ryan <seamusryan787@gmail.com>
2026-01-10 17:47:55 -0500

    src

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Header.css";
-import "..ToggleSwitch/ToggleSwitch";
-import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";  // keep this one
 
 function Header({ handleAddClick, weatherData }) {
   const temp = weatherData?.temp?.F ?? "--";
@@ -15,28 +14,23 @@ function Header({ handleAddClick, weatherData }) {
   return (
     <header className="header">
       <div className="header__content">
-
-        {/* Weather Header */}
         <div className="header__weather-header">
           <span className="header__date">{dateString}</span>
           <span className="header__location">in {city}</span>
           <span className="header__temp-large">{temp}°F</span>
         </div>
 
-        {/* App Title */}
         <h1 className="header__logo">WTWR — What To Wear</h1>
 
-        {/* User Info */}
         <p className="header__username">Terrence Tegegne</p>
         <div className="header__avatar">TT</div>
 
-        {/* Add Garment Button */}
         <button
           type="button"
           className="header__add-button"
           onClick={handleAddClick}
         >
-          <ToggleSwitch/>
+          <ToggleSwitch />
           + Add Clothes
         </button>
       </div>
