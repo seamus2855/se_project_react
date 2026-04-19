6cd9ca91957fe0f58ef31755e32fed89f6842a46
Seamus Ryan <seamusryan787@gmail.com>
2026-04-19 13:09:47 -0400

    sprint 14

--- src/WTWR Final/se_project_react/src/contexts/CurrentTemperatureUnitContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const CurrentTemperatureUnitContext = createContext({
-  currentTemperatureUnit: "F",
-  handleToggleSwitchChange: () => {},
-});
-
-export default CurrentTemperatureUnitContext;
